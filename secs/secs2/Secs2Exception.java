e2f0a5ba413fee2755d47844d8bde20c8751a41a
shimizu.kenta <shimizukentagm1@gmail.com>
2019-03-23 02:50:00 +0900

    under-construction

---
+++ secs/secs2/Secs2Exception.java
@@ -0,0 +1,27 @@
+package secs.secs2;
+
+public class Secs2Exception extends Exception {
+
+	private static final long serialVersionUID = 4357717366163333969L;
+
+	public Secs2Exception() {
+		super();
+	}
+
+	public Secs2Exception(String message) {
+		super(message);
+	}
+
+	public Secs2Exception(Throwable cause) {
+		super(cause);
+	}
+
+	public Secs2Exception(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public Secs2Exception(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
