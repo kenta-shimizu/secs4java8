883b68422d77c91e39bfad2086b270279ba61765
shimizu.kenta <shimizukentagm1@gmail.com>
2020-03-09 19:24:56 +0900

    2020-03-09T19:24

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.shimizukenta.secs {
+	exports com.shimizukenta.secs;
+	exports com.shimizukenta.secs.gem;
+	exports com.shimizukenta.secs.hsmsss;
+	exports com.shimizukenta.secs.secs1;
+	exports com.shimizukenta.secs.secs1ontcpip;
+	exports com.shimizukenta.secs.secs2;
+	exports com.shimizukenta.secs.sml;
+}
